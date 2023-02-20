261c37d802b390a3b40f13a5146f114eec5411a1
Mark Murphy <murphy.contact@aol.com>
2023-02-19 19:34:27 -0500

    CHIME-1015: Add possible use cases

--- src/Modules/Users/Chime.Modules.Users.Api/Controllers/AccountController.cs
+++ src/Modules/Users/Chime.Modules.Users.Api/Controllers/AccountController.cs
@@ -1,6 +1,7 @@
 using Chime.Modules.Users.Core.Commands.SignUp;
 using Chime.Shared.Abstractions.Contexts;
 using Chime.Shared.Abstractions.Dispatchers;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Swashbuckle.AspNetCore.Annotations;
@@ -20,12 +21,22 @@ internal class AccountController : BaseController
     public AccountController(IDispatcher dispatcher, IContext context
         // IUserRequestStorage userRequestStorage,
         // CookieOptions cookieOptions
-        )
+    )
     {
         _dispatcher = dispatcher;
         _context = context;
         // _userRequestStorage = userRequestStorage;
         // _cookieOptions = cookieOptions;
+    }
+
+    [HttpGet]
+    [Authorize]
+    [SwaggerOperation("Get account")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    public async Task<ActionResult> GetAsync()
+    {
+        return Ok();
     }
 
     [HttpPost("sign-up")]
@@ -35,6 +46,26 @@ internal class AccountController : BaseController
     public async Task<ActionResult> SignUpAsync(SignUp command)
     {
         await _dispatcher.SendAsync(command);
+        return NoContent();
+    }
+
+    [HttpPost("sign-in")]
+    [SwaggerOperation("Sign in")]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public async Task<ActionResult> SignInAsync()
+    {
+        return Ok();
+    }
+
+    [Authorize]
+    [HttpDelete("sign-out")]
+    [SwaggerOperation("Sign out")]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    public async Task<ActionResult> SignOutAsync()
+    {
         return NoContent();
     }
 }
--- src/Modules/Users/Chime.Modules.Users.Api/Controllers/UsersController.cs
+++ src/Modules/Users/Chime.Modules.Users.Api/Controllers/UsersController.cs
@@ -1,5 +1,8 @@
 using Chime.Shared.Abstractions.Dispatchers;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
 
 namespace Chime.Modules.Users.Api.Controllers;
 
@@ -12,5 +15,37 @@ internal class UsersController : BaseController
     public UsersController(IDispatcher dispatcher)
     {
         _dispatcher = dispatcher;
+    }
+
+    [HttpGet("{userId:guid}")]
+    [SwaggerOperation("Get user")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(StatusCodes.Status403Forbidden)]
+    public async Task<ActionResult> GetAsync()
+    {
+        return Ok();
+    }
+
+    [HttpGet]
+    [SwaggerOperation("Browse users")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(StatusCodes.Status403Forbidden)]
+    public async Task<ActionResult> BrowseAsync()
+    {
+        return Ok();
+    }
+
+    [HttpPut("{userId:guid}/state")]
+    [SwaggerOperation("Update user state")]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(StatusCodes.Status403Forbidden)]
+    public async Task<ActionResult> UpdateStateAsync()
+    {
+        return NoContent();
     }
 }
